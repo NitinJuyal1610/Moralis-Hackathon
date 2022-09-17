521c19630e74624e0e302fb9a82572c2679e7544
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2022-09-18 00:19:49 +0530

    Nominee Form

--- client/pages/Profile.jsx
+++ client/pages/Profile.jsx
@@ -4,7 +4,7 @@ import "react-image-upload/dist/index.css";
 import Navbar from "./components/Navbar";
 import styles from "../styles/profile.module.css";
 import { FileUpload } from "./components/FileUpload";
-import InsureContext from "./context/InsureContext";
+
 const Profile = () => {
   const { user } = useMoralis();
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
@@ -18,43 +18,56 @@ const Profile = () => {
   };
 
   const [formData, setFormData] = useState({
-    fullName: "",
     username: "",
     email: "",
     phoneNumber: "",
-    gender: "",
-    password: "",
-    ConPassword: "",
+    NomineeName: "",
+    NomineeAddress: "",
   });
-  const {
-    fullName,
-    username,
-    email,
-    phoneNumber,
-    gender,
-    password,
-    ConPassword,
-  } = formData;
+  const { username, email, phoneNumber, NomineeName, NomineeAddress } =
+    formData;
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const handleSubmit = (e) => {
+  const UpdateInfo = (e) => {
     e.preventDefault();
-    if (password === ConPassword) {
-      console.log(
-        username,
-        password,
-        email,
-        { phoneNumber: phoneNumber },
-        { gender: gender },
-        { fullName: fullName }
-      );
-      // signup(username, password, email, {
-      //   phoneNumber: phoneNumber,
-      //   gender: gender,
-      //   fullName: fullName,
-      // });
+    console.log(username, email, { phoneNumber: phoneNumber });
+
+    if (username.length) {
+      user.set("username", username);
     }
+
+    if (phoneNumber.length) {
+      user.set("phoneNumber", phoneNumber);
+    }
+
+    if (email.length) {
+      user.set("email", email);
+    }
+
+    user.save();
+  };
+
+  const NomineeInfo = (e) => {
+    e.preventDefault();
+    console.log(NomineeName, NomineeAddress);
+    if (NomineeAddress.length) {
+      if (user?.get("NomineeAddress")) {
+        user.set("NomineeAddress", NomineeAddress);
+      } else {
+        user.addUnique("NomineeAddress", NomineeAddress);
+      }
+    }
+
+    if (NomineeName.length) {
+      if (user?.get("NomineeName")) {
+        user.set("NomineeName", NomineeName);
+      } else {
+        user.addUnique("NomineeName", NomineeName);
+      }
+    }
+
+    user.save();
   };
 
   return (
@@ -87,7 +100,7 @@ const Profile = () => {
               )}
             </div>
 
-            <h3 className={styles.name}>Name : {user?.get("fullName")}</h3>
+            <h3 className={styles.name}>Name : {user?.get("username")}</h3>
             <h3 className={styles.gender}>Gender : {user?.get("gender")}</h3>
             <h3 className={styles.phoneNumber}>
               Mobile No. : {user?.get("phoneNumber")}
@@ -100,22 +113,10 @@ const Profile = () => {
           <div className={styles.form_box}>
             <form
               className={styles.form}
-              onSubmit={(e) => handleSubmit(e)}
+              onSubmit={(e) => UpdateInfo(e)}
               action="#"
             >
               <div className={styles.user_details}>
-                <div className={styles.input_box}>
-                  <span className={styles.details}>Full Name</span>
-                  <input
-                    className={styles.input}
-                    type="text"
-                    placeholder="Enter your name"
-                    name="fullName"
-                    value={fullName}
-                    onChange={(e) => onChange(e)}
-                    disabled={isButtonDisabled}
-                  />
-                </div>
                 <div className={styles.input_box}>
                   <span className={styles.details}>Username</span>
                   <input
@@ -123,7 +124,7 @@ const Profile = () => {
                     type="text"
                     name="username"
                     value={username}
-                    placeholder="Enter your username"
+                    placeholder={user?.get("username")}
                     onChange={(e) => onChange(e)}
                     disabled={isButtonDisabled}
                   />
@@ -135,7 +136,7 @@ const Profile = () => {
                     type="text"
                     name="email"
                     value={email}
-                    placeholder="Enter your email"
+                    placeholder={user?.get("email")}
                     onChange={(e) => onChange(e)}
                     disabled={isButtonDisabled}
                   />
@@ -147,32 +148,7 @@ const Profile = () => {
                     type="text"
                     name="phoneNumber"
                     value={phoneNumber}
-                    placeholder="Enter your number"
-                    onChange={(e) => onChange(e)}
-                    disabled={isButtonDisabled}
-                  />
-                </div>
-
-                <div className={styles.input_box}>
-                  <span className={styles.details}>Password</span>
-                  <input
-                    className={styles.input}
-                    type="password"
-                    name="password"
-                    value={password}
-                    placeholder="Enter your password"
-                    onChange={(e) => onChange(e)}
-                    disabled={isButtonDisabled}
-                  />
-                </div>
-                <div className={styles.input_box}>
-                  <span className={styles.details}>Confirm Password</span>
-                  <input
-                    className={styles.input}
-                    type="password"
-                    name="ConPassword"
-                    value={ConPassword}
-                    placeholder="Confirm your password"
+                    placeholder={user?.get("phoneNumber")}
                     onChange={(e) => onChange(e)}
                     disabled={isButtonDisabled}
                   />
@@ -180,7 +156,7 @@ const Profile = () => {
               </div>
 
               <div className={styles.button}>
-                <input type="submit" value="Register" />
+                <input type="submit" value="Save" />
               </div>
               <button className={styles.authButton} onClick={enable}>
                 Edit
@@ -191,7 +167,7 @@ const Profile = () => {
         <div className={styles.right}>
           <form
             className={styles.form}
-            onSubmit={(e) => handleSubmit(e)}
+            onSubmit={(e) => NomineeInfo(e)}
             action="#"
           >
             <div className={styles.user_details}>
@@ -200,33 +176,22 @@ const Profile = () => {
                 <input
                   className={styles.input}
                   type="text"
-                  placeholder="Enter Nominee name"
-                  name="nomineefullName"
-                  // value={nomineefullName}
+                  placeholder={user?.get("NomineeName")}
+                  name="NomineeName"
+                  value={NomineeName}
                   onChange={(e) => onChange(e)}
                   disabled={isButtonDisabled}
                 />
               </div>
+
               <div className={styles.input_box}>
-                <span className={styles.details}>Nominee Email</span>
+                <span className={styles.details}>Nominee Wallet Address</span>
                 <input
                   className={styles.input}
                   type="text"
-                  name="nomineeemail"
-                  // value={nomineeemail}
-                  placeholder="Enter Nominee email"
-                  onChange={(e) => onChange(e)}
-                  disabled={isButtonDisabled}
-                />
-              </div>
-              <div className={styles.input_box}>
-                <span className={styles.details}>Nominee Phone Number</span>
-                <input
-                  className={styles.input}
-                  type="text"
-                  name="nomineephoneNumber"
-                  // value={nomineephoneNumber}
-                  placeholder="Enter Nominee number"
+                  name="NomineeAddress"
+                  value={NomineeAddress}
+                  placeholder={user?.get("NomineeAddress")}
                   onChange={(e) => onChange(e)}
                   disabled={isButtonDisabled}
                 />
@@ -239,19 +204,7 @@ const Profile = () => {
           </form>
         </div>
       </div>
-      <div>
-        {/* <Link href="/Login">
-          <button
-            onClick={() => {
-              logout();
-              console.log(isAuthenticated);
-            }}
-            disabled={isAuthenticating}
-          >
-            Logout{console.log(isAuthenticated)}
-          </button>
-        </Link> */}
-      </div>
+      <div></div>
     </div>
   );
 };

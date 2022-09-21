9e888de605c0485da46cd691c1eed334c73573cd
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2022-09-21 23:47:53 +0530

    some changes

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -27,84 +27,82 @@ const Login = () => {
     }
   }, []);
 
-  if (isWeb3EnableLoading) return <>loading</>;
-  else
-    return (
-      <>
-        <Navbar />
-        {isAuthenticated ? (
-          <>
-            <div>
-              <form className={styles.login_form}>
-                <h1>Login</h1>
-                <div className={styles.content}>
-                  <div className={styles.input_field}>
-                    <input
-                      type="email"
-                      placeholder="Email"
-                      onChange={(e) => {
-                        setUsername(e.target.value);
-                      }}
-                      autocomplete="nope"
-                    />
-                  </div>
-                  <div className={styles.input_field}>
-                    <input
-                      type="password"
-                      placeholder="Password"
-                      onChange={(e) => {
-                        setPassword(e.target.value);
-                      }}
-                      autocomplete="new-password"
-                    />
-                  </div>
-                </div>
-                <div className={styles.action}>
-                  <Link href="/Register" className={styles.User_Route}>
-                    <button>Register</button>
-                  </Link>
-                  <button
-                    onClick={() => {
-                      try {
-                        login(username, password, {
-                          onError: () => {
-                            alert("Wrong Username/Password");
-                            throw "Wrong Username/Password";
-                          },
-
-                          onSuccess: () => {
-                            setTimeout(function () {
-                              router.push("/Profile");
-                            }, 1000);
-                          },
-                        });
-                      } catch (error) {
-                        console.log(error);
-                      }
+  return (
+    <>
+      <Navbar />
+      {isAuthenticated ? (
+        <>
+          <div>
+            <form className={styles.login_form}>
+              <h1>Login</h1>
+              <div className={styles.content}>
+                <div className={styles.input_field}>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e) => {
+                      setUsername(e.target.value);
                     }}
-                  >
-                    Sign in
-                  </button>
+                    autocomplete="nope"
+                  />
                 </div>
-              </form>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className={styles.body}>
-              <button
-                className={styles.authButton}
-                onClick={() =>
-                  authenticate({ signingMessage: "Moralis Authentication" })
-                }
-              >
-                Authenticate
-              </button>
-            </div>
-          </>
-        )}
-      </>
-    );
+                <div className={styles.input_field}>
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    autocomplete="new-password"
+                  />
+                </div>
+              </div>
+              <div className={styles.action}>
+                <Link href="/Register" className={styles.User_Route}>
+                  <button>Register</button>
+                </Link>
+                <button
+                  onClick={() => {
+                    try {
+                      login(username, password, {
+                        onError: () => {
+                          alert("Wrong Username/Password");
+                          throw "Wrong Username/Password";
+                        },
+
+                        onSuccess: () => {
+                          setTimeout(function () {
+                            router.push("/Profile");
+                          }, 1000);
+                        },
+                      });
+                    } catch (error) {
+                      console.log(error);
+                    }
+                  }}
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={styles.body}>
+            <button
+              className={styles.authButton}
+              onClick={() =>
+                authenticate({ signingMessage: "Moralis Authentication" })
+              }
+            >
+              Authenticate
+            </button>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Login;

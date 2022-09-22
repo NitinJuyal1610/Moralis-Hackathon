5ff269afa54e028820ee41ba125c3db8d153a58f
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2022-09-23 00:42:19 +0530

    fix

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -192,7 +192,6 @@ const Profile = () => {
                   <input
                     className={styles.input}
                     type="text"
-                    placeholder={user?.get("NomineeName")[0]}
                     name="NomineeName"
                     value={NomineeName}
                     onChange={(e) => onChange(e)}
@@ -207,7 +206,6 @@ const Profile = () => {
                     type="text"
                     name="NomineeAddress"
                     value={NomineeAddress}
-                    placeholder={user?.get("NomineeAddress")[0]}
                     onChange={(e) => onChange(e)}
                     disabled={isButtonDisabled}
                   />
